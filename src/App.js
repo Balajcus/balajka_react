fb37a2f9c1952cf2988fb6c25f0cb3283aa3f4bb
moucka <s2024MouckaSebastian@skolabaltaci.cz>
2025-03-14 08:44:54 +0100

    ahoj

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
+import About from './pages/about';
+import Home from './pages/about';
+import {Routes, Route} from 'react-router';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Routes>
+        <Route paht="/" element={Home}></Route>
+        <Route paht="/about" element={About}></Route>
+      </Routes>
   );
 }
 
---
+++ src/pages/about.js
@@ -0,0 +1,9 @@
+export default function About(){
+    return<>
+    <div>
+        <h1>sdadsfsdsgdf</h1>
+
+
+    </div>
+    </>
+}
